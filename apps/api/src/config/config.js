ae220015ed9b8d777f41bb39521da34410fa66d1
Ganesh07A <work.ganeshsuvarnkar@gmail.com>
2026-03-16 22:48:27 +0530

    feat: db schema created

---
+++ apps/api/src/config/config.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+if(!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in environment variables")
+}
+const config= {
+    DATABASE_URL: process.env.DATABASE_URL
+}
+export default config;
