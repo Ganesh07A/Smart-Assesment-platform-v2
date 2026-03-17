e5221a92c7c96ead47dec00d2d2e45e267dccad1
Ganesh07A <work.ganeshsuvarnkar@gmail.com>
2026-03-17 21:48:47 +0530

    fix: prisma version issue fixed

--- apps/api/src/config/config.js
+++ apps/api/src/config/config.js
@@ -1,11 +1,11 @@
-const dotenv = require("dotenv")
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+dotenv.config();
+// Initialize the Postgres adapter
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-dotenv.config()
-
-if(!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is not defined in environment variables")
-}
-const config= {
-    DATABASE_URL: process.env.DATABASE_URL
-}
-export default config;
+// Pass the adapter to PrismaClient
+export const prisma = new PrismaClient({ adapter });
